12a617f395d400524af5adf3889c810a0f748d61
Ruize Ma <yibagaozi@outlook.com>
2026-01-13 12:11:51 +0800

    Add QR code models, config, and exception class
    Introduced QRCodeProperties for configuration and added models for QR code generation and decoding requests and responses. Added BarcodeException for error handling. Updated pom.xml to include ZXing dependencies for QR code processing.

---
+++ barcode-core/src/main/java/com/soilmate/barcodecore/config/QRCodeProperties.java
@@ -0,0 +1,17 @@
+package com.soilmate.barcodecore.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+//@ConfigurationProperties(prefix="soilmate.qrcode")
+public class QRCodeProperties {
+
+    private int width = 300;
+    private int height = 300;
+    private String charset = "UTF-8";
+    private int margin = 1;
+    private String errorCorrectionLevel = "M";
+    private String imageFormat = "PNG";
+
+}
