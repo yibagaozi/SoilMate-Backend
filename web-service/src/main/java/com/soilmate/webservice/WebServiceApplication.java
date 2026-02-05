2f33086cd5f4d2531eb53335a0a8b166cf6c6d2c
Ruize Ma <yibagaozi@outlook.com>
2026-02-05 18:55:09 +0800

    fix bugs

--- web-service/src/main/java/com/soilmate/webservice/WebServiceApplication.java
+++ web-service/src/main/java/com/soilmate/webservice/WebServiceApplication.java
@@ -2,8 +2,10 @@ package com.soilmate.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.soilmate.barcodeservice", "com.soilmate.barcodecore", "com.soilmate.common", "com.soilmate.webservice"})
 public class WebServiceApplication {
 
     public static void main(String[] args) {
